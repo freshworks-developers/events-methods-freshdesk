393cd2c9a50ef8a852c8af5dbf2521e558730626
Saif Ali Shaik <saifshine7@gmail.com>
2021-03-04 17:19:24 +0530

    👌 IMPROVE: click events captured

---
+++ app/scripts/cti.js
@@ -0,0 +1,24 @@
+document.onreadystatechange = function () {
+  if (document.readyState === 'interactive') renderApp();
+};
+
+function renderApp() {
+  app
+    .initialized()
+    .then((_client) => {
+      window['client'] = _client;
+      client.events.on('app.activated', phoneClickListener);
+    })
+    .catch(console.error);
+}
+
+function phoneClickListener() {
+  /** ~ play ground starts here ~ */
+  client.events.on('cti.triggerDialer', function renderPhoneNumber(event) {
+    const { number } = event.helper.getData();
+    const label = document.querySelector('.spotlight');
+    label.setAttribute('value', `Phone Number: ${number}`);
+    console.log(number);
+  });
+  /** ~ end ~ */
+}
---
+++ app/scripts/tkt_req_info.js
@@ -0,0 +1,40 @@
+document.onreadystatechange = function () {
+  if (document.readyState === 'interactive') renderApp();
+};
+
+function renderApp() {
+  app
+    .initialized()
+    .then((_client) => {
+      window['client'] = _client;
+      client.events.on('app.activated', eventsInTktDetailsPage);
+    })
+    .catch(console.error);
+}
+
+function eventsInTktDetailsPage() {
+  const spotlight = document.querySelector('.spotlight');
+  let clickEvents = [
+    'ticket.replyClick',
+    'ticket.sendReply',
+    'ticket.forwardClick',
+    'ticket.conversationForward',
+    'ticket.forward',
+    'ticket.notesClick',
+    'ticket.addNote',
+    'ticket.closeTicketClick',
+    'ticket.deleteTicketClick',
+    'ticket.previousTicketClick',
+    'ticket.nextTicketClick',
+    'ticket.startTimer',
+    'ticket.stopTimer',
+    'ticket.updateTimer',
+    'ticket.deleteTimer'
+  ];
+
+  clickEvents.forEach(function register(click) {
+    client.events.on(click, function writeToDOM() {
+      spotlight.insertAdjacentHTML('afterend', `<fw-label value="${click}" color="green"></fw-label>`);
+    });
+  });
+}
