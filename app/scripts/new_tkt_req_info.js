c2123148bbb444fbb4a68739cfc3fa55e14963c0
Saif Ali Shaik <saifshine7@gmail.com>
2021-03-05 15:01:06 +0530

    🐛 FIX: stage the start

--- app/scripts/new_tkt_req_info.js
+++ app/scripts/new_tkt_req_info.js
@@ -12,29 +12,11 @@ function renderApp() {
     .catch(console.error);
 }
 
+/**
+ * Listens to changes events and writes them to the app UI.
+ */
+
 function registerChangeEvents() {
-  const spotlight = document.querySelector('.spotlight');
-
-  let changeEvents = [
-    'ticket.priorityChanged',
-    'ticket.statusChanged',
-    'ticket.groupChanged',
-    'ticket.agentChanged',
-    'ticket.typeChagned'
-  ];
-
-  changeEvents.forEach(function register(click) {
-    client.events.on(click, function writeToDOM(event) {
-      let eventName = event.type;
-      let { old: prevVal, new: newVal } = event.helper.getData();
-      const row = `
-          <tr>
-            <td>${prevVal}</td>
-            <td>${newVal}</td>
-            <td>${eventName.slice(7)}</td>
-          </tr>
-      `;
-      spotlight.insertAdjacentHTML('beforeend', row);
-    });
-  });
+  /** ~ play ground starts here ~ */
+  /** ~ end ~ */
 }
--- app/scripts/tkt_req_info.js
+++ app/scripts/tkt_req_info.js
@@ -12,55 +12,11 @@ function renderApp() {
     .catch(console.error);
 }
 
+/**
+ * Observes ticket click events and intercept events
+ */
+
 function eventsInTktDetailsPage() {
-  const spotlight = document.querySelector('.spotlight');
-
-  let clickEvents = [
-    'ticket.replyClick',
-    'ticket.sendReply',
-    'ticket.forwardClick',
-    'ticket.conversationForward',
-    'ticket.forward',
-    'ticket.notesClick',
-    'ticket.addNote',
-    'ticket.closeTicketClick',
-    'ticket.deleteTicketClick',
-    'ticket.previousTicketClick',
-    'ticket.nextTicketClick',
-    'ticket.startTimer',
-    'ticket.stopTimer',
-    'ticket.updateTimer',
-    'ticket.deleteTimer'
-  ];
-
-  let interceptEvents = {
-    prevent: ['ticket.closeTicketClick', 'ticket.deleteTicketClick'],
-    allow: ['ticket.propertiesUpdated', 'ticket.sendReply']
-  };
-
-  clickEvents.forEach(function register(click) {
-    client.events.on(click, function writeToDOM(event) {
-      spotlight.insertAdjacentHTML('afterend', `<fw-label value="${click}" color="green"></fw-label>`);
-    });
-  });
-
-  interceptEvents['prevent'].forEach(function registerCb(click) {
-    client.events.on(click, preventClickEvent, { intercept: true });
-
-    function preventClickEvent(event) {
-      let eventName = event.type;
-      const row = `<fw-label value="${eventName.slice(7)} prevented" color="red"></fw-label>`;
-      spotlight.insertAdjacentHTML('afterend', row);
-    }
-  });
-
-  interceptEvents['allow'].forEach(function registerCb(click) {
-    client.events.on(click, allowClickEvents);
-
-    function allowClickEvents(event) {
-      let eventName = event.type;
-      const row = `<fw-label value="${eventName.slice(7)} allowed" color="red"></fw-label>`;
-      spotlight.insertAdjacentHTML('afterend', row);
-    }
-  });
+  /** ~ play ground starts here ~ */
+  /** ~ end ~ */
 }
