fe0bef51aab09ef949586d2dc009acc00dca1097
Saif Ali Shaik <saifshine7@gmail.com>
2021-03-05 18:10:39 +0530

    🚀 RELEASE: click events

--- app/scripts/tkt_req_info.js
+++ app/scripts/tkt_req_info.js
@@ -18,5 +18,30 @@ function renderApp() {
 
 function eventsInTktDetailsPage() {
   /** ~ play ground starts here ~ */
+  const spotlight = document.querySelector('.spotlight');
+
+  let clickEvents = [
+    'ticket.replyClick',
+    'ticket.sendReply',
+    'ticket.forwardClick',
+    'ticket.conversationForward',
+    'ticket.forward',
+    'ticket.notesClick',
+    'ticket.addNote',
+    'ticket.closeTicketClick',
+    'ticket.deleteTicketClick',
+    'ticket.previousTicketClick',
+    'ticket.nextTicketClick',
+    'ticket.startTimer',
+    'ticket.stopTimer',
+    'ticket.updateTimer',
+    'ticket.deleteTimer'
+  ];
+  clickEvents.forEach(function register(click) {
+    client.events.on(click, function writeToDOM(event) {
+      spotlight.insertAdjacentHTML('afterend', `<fw-label value="${click}" color="green"></fw-label>`);
+    });
+  });
+
   /** ~ end ~ */
 }
