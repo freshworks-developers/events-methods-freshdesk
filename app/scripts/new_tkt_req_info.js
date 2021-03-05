6537bdd74cd866703b3285e7849fb445dfdbfe72
Saif Ali Shaik <saifshine7@gmail.com>
2021-03-05 18:13:14 +0530

    🚀 RELEASE: change-events

--- app/scripts/new_tkt_req_info.js
+++ app/scripts/new_tkt_req_info.js
@@ -18,5 +18,30 @@ function renderApp() {
 
 function registerChangeEvents() {
   /** ~ play ground starts here ~ */
+  const spotlight = document.querySelector('.spotlight');
+
+  let changeEvents = [
+    'ticket.priorityChanged',
+    'ticket.statusChanged',
+    'ticket.groupChanged',
+    'ticket.agentChanged',
+    'ticket.typeChagned'
+  ];
+
+  changeEvents.forEach(function register(click) {
+    client.events.on(click, function writeToDOM(event) {
+      let eventName = event.type;
+      let { old: prevVal, new: newVal } = event.helper.getData();
+      const row = `
+          <tr>
+            <td>${prevVal}</td>
+            <td>${newVal}</td>
+            <td>${eventName.slice(7)}</td>
+          </tr>
+      `;
+      spotlight.insertAdjacentHTML('beforeend', row);
+    });
+  });
+
   /** ~ end ~ */
 }
