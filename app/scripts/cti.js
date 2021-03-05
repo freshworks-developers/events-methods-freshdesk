cd148ab31fd586c9ba3b3091ea7b4fcbdb7cde89
Saif Ali Shaik <saifshine7@gmail.com>
2021-03-05 13:00:16 +0530

    📖 DOC: desc phoneclicklistener

--- app/scripts/cti.js
+++ app/scripts/cti.js
@@ -12,6 +12,11 @@ function renderApp() {
     .catch(console.error);
 }
 
+/**
+ * Registers a callback when user clicks on phone number in Freshdesk.
+ * The callback appends phone number as a label to the app's user interface.
+ */
+
 function phoneClickListener() {
   /** ~ play ground starts here ~ */
   client.events.on('cti.triggerDialer', function renderPhoneNumber(event) {
